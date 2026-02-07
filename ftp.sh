2271e31f08187d9e606f58fbc91b59ea9f749698
AOUADENE Adel <aouadeneadel@outlook.fr>
2026-02-07 17:49:34 +0100

    Add files via upload

---
+++ ftp.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+. main.conf
+
+logfiles=''
+
+for i in $(ls $logpath)
+do
+	logfiles="${logfiles}put $logpath/$i"
+	logfiles+=$'\n'
+done
+echo -e $logfiles
+
+
+lftp -u $ftpuser,$ftppassword $ftphost <<EOF
+set ssl:verify-certificate no
+cd $ftpdirectory
+$logfiles
+bye
+EOF
